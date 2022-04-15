271334e28ddc71754e367a8e4d8f5f48e6f8c210
Aditi <aditishandilya00@gmail.com>
2022-04-14 20:32:08 -0400

    Adding server switched to HTTP and can decode JSON

--- Python Client Server/httpserver.py
+++ Python Client Server/httpserver.py
@@ -1,76 +1,106 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import socketserver
+# from http.server import HTTPServer, BaseHTTPRequestHandler
+# import socketserver
 
-# Function to define the behavior of each thread/client
-def fileTransferThread(connection):
-    while True:
-        # Obtain request details from client side and call proper sections
-        #messageFromClient = connection.recv(1024).decode()
-        messageFromClient = connection.recv(1024)
-        messageFromClient = gzip.decompress(messageFromClient).decode('utf-8')
-        #messageFromClient = str(connection.recv(1024))
+# # Driver code for server
+# myServer = HTTPServer(server_address=('localhost',8080), RequestHandlerClass=BaseHTTPRequestHandler)
+# print("\nWelcome to the TikEdu server!\n")
+# myServer.serve_forever()
 
-        print("messageFromClient: " + messageFromClient)
-        if messageFromClient == "exit":
-            print("Client has disconnected.\n")
-            break
 
-        try:
-            myCommand = messageFromClient.split()[0]
-            fileName = messageFromClient.split()[1]
-        except: 
-            print("Message from client222: ", messageFromClient)
-            return 
 
-        if myCommand == "get":
 
-            # Print message on server side terminal
-            print("File to be downloaded =", fileName)
 
-            # Open the file to download
-            myFile = open(fileName, "rb")
 
-            # Read from file and send data to the client until no data is left
-            while True:
-                dataToSend = myFile.read(1024)
-                if not dataToSend:
-                    break
-                connection.send(dataToSend)
-                if len(dataToSend) < 1024:
-                    break
 
-            # Close the file
-            myFile.close()
 
-            # Print message on server side terminal to indicate file transfer complete
-            print("File has been downloaded successfully. \nPlease return to the client or connect a new client to this server. \n")
 
-        elif myCommand == "upload":
+# # Import libraries
+# import http.server
+# import socketserver
+# import json
+  
+# # Defining PORT number
+# #HOST, PORT = "192.168.1.9", 8080
+# HOST, PORT = "localhost", 8080
+# # Creating handle
+# Handler = http.server.SimpleHTTPRequestHandler
+  
+# # Creating TCPServer
+# http = socketserver.TCPServer((HOST, PORT), Handler)
+  
+# # Getting logs
+# print("serving at port", PORT)
 
-             # Print message on server side terminal
-            print("File to be uploaded =", fileName)
+# # Get json from the client
+# def getJson(messageFromClient):
+#     # Convert the message from client to json
+#     jsonMessage = json.loads(messageFromClient)
+#     # Return the json message
+#     return jsonMessage
 
-            # Open a file to write to with proper name
-            newFilename = "new" + fileName
-            myFile = open(newFilename, "wb")
 
-            # Receive data from the client and write to file until no more data is received
-            while True:
-                receivedData = connection.recv(1024)
-                if not receivedData:
-                    break
-                myFile.write(receivedData)
-                if len(receivedData) < 1024:
-                    break
 
-            # Once file has been downloaded completely, close file
-            myFile.close()
+# http.serve_forever()
 
-            # Print message on server side terminal to indicate file transfer complete
-            print("File has been uploaded successfully. \nPlease return to the client or connect a new client to this server. \n")
 
-    
-# Driver code for server
-myServer = HTTPServer(server_address=('localhost',8080), RequestHandlerClass=BaseHTTPRequestHandler)
-print("\nWelcome to the TikEdu server!\n")
-myServer.serve_forever()
+
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+from sre_constants import SUCCESS
+import json
+
+global myJ
+myS = {'text': 'Server get request'}
+myJ = json.dumps(myS)
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        global myJ
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write(myJ.encode('utf-8'))
+        #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+                #gets the json data from the client
+                #str(self.path), str(self.headers), getJson(post_data.decode('utf-8')))
+
+        myjson = json.loads(post_data.decode('utf-8'))
+        print("My req is")
+        #print(myjson['text'])
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, PORT=8080, HOST = "localhost"):
+    logging.basicConfig(level=logging.INFO)
+    server_address = (HOST, PORT)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        print("SUCCESS")
+        httpd.serve_forever()
+        print()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+
+from sys import argv
+
+
+PORT = 8080
+HOST = "192.168.1.9"
+run(PORT = PORT, HOST = HOST)
