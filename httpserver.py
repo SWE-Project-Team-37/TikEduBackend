1cd9b333bc4acfbe72a00a694263bc1ff461f599
collingee <collingee@ufl.edu>
2022-04-20 19:47:47 -0400

    Added the correct http server back in

--- httpserver.py
+++ httpserver.py
@@ -2,57 +2,78 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from io import SEEK_SET
 from sre_constants import SUCCESS
 import json
+from colorama import Cursor
 from matplotlib.font_manager import json_dump
+import ServerCode
+global myJson
 
-global myJ
+global connection
+global cursor
+
+connection = ServerCode.get_connection()
+connection.autocommit = True
+cursor = ServerCode.get_cursor(connection)
 
 # Sign Up Implementation
 def configureJSON_signup(signUpIsSuccessful,userAccessToken,errorMessage):
-    global myJ
-    myJ = json.dumps({'signUpIsSuccessful': signUpIsSuccessful, 'userAccessToken': userAccessToken, 'errorMessage': errorMessage})
+    global myJson
+    myJson = json.dumps({'signUpIsSuccessful': signUpIsSuccessful, 'userAccessToken': userAccessToken, 'errorMessage': errorMessage})
 
 def signupLogic(username,password,usertype):
     # Call function checkUserExists()
+    global connection
+    global cursor
+    exists = ServerCode.checkUserExists(cursor,username,password)
+
     # Above function will return true if the username/password combo already exists in the database
 
-    # global myJ
+    global myJson
 
     # If checkUserExists() returns true, return error message - signup failed 
         # configureJSON_signup(False,"-1","Error with Sign Up - Username already exists")
         # return
+    if exists:
+        configureJSON_signup(False,"-1","Error with Sign Up - Username already exists")
+        return
+    else:
+        ServerCode.addUser(cursor,usertype,username,password)
+        userID = ServerCode.getUserID(cursor,username,password)
+        
     # If checkUserExists() returns false, INSERT new user into database
         # INSERT statement - exactly as it would be in SQL Developer
         # SELECT statement - to obtain userID of this username/password combo
-        # configureJSON_signup(True,userID,"")
+        configureJSON_signup(True,userID,"")
         return
 
 # Sign In Implementation
 def configureJSON_signin(signInIsSuccessful,userAccessToken,errorMessage):
-    global myJ
-    myJ = json.dumps({'signInIsSuccessful': signInIsSuccessful, 'userAccessToken': userAccessToken, 'errorMessage': errorMessage})
+    global myJson
+    myJson = json.dumps({'signInIsSuccessful': signInIsSuccessful, 'userAccessToken': userAccessToken, 'errorMessage': errorMessage})
 
 def signinLogic(username,password):
-    # Call function checkUserExists()
+    global connection
+    global cursor
+    global myJson
+    exists = ServerCode.checkUserExists(cursor,username,password)
     # Above function will return true if the username/password combo already exists in the database
 
-    # global myJ
-
-    # If checkUserExists() returns false, return error message - signin failed 
-        # configureJSON_signin(False,"-1","Error with Sign In - Given password is not associated with this username")
-        # return
-    # If checkUserExists() returns true, return userID
-        # SELECT statement - to obtain userID of this username/password combo
-        # configureJSON_signup(True,userID,"")
+    # If checkUserExists() returns false, return error message - signup failed 
+    if not exists:
+        configureJSON_signin(False,"-1","Error with Sign In - Username/password doesn't exist")
+        return
+    else:
+        userID = ServerCode.getUserID(cursor,username,password)
+        configureJSON_signin(True,userID,"")
         return
 
 # Send All User Data Implementation
 def configureJSON_sendAllUserData(userData):
-    global myJ
+    global myJson
     # myJ = json.dumps({'userDataSentSuccessfully': userData[0], 'usertype': userData[1], 'userID': userData[2], 'username': userData[3], 'tags_rank': userData[4], 'tags_names': userData[5], 'hashedPassword': userData[6], 'errorMessage': userData[7]})
     return
 
 def sendAllUserDataLogic(userID):
-    global myJ
+    global myJson
     userData = []
     # SELECT statement to find user with given userID in the database
     # save resullt of above statement to usertype, username, tags_rank, tags_names, hashedPassword
@@ -80,13 +101,13 @@ def sendAllUserDataLogic(userID):
 
 # User Associated Classes Implementation
 def configureJSON_UserAssociatedClasses(isSuccessful,classes,errorMessage):
-    global myJ
+    global myJson
     # classes is a list
-    myJ = json.dumps({'userAssociatedClassesSuccessful:': isSuccessful, 'userClasses': classes, 'errorMessage': errorMessage})
+    myJson = json.dumps({'userAssociatedClassesSuccessful:': isSuccessful, 'userClasses': classes, 'errorMessage': errorMessage})
     return
 
 def userAssociatedClassesLogic(userID):
-    global myJ
+    global myJson
     # SELECT statement to find user with given userID in the database
     # if userID found:
         # isSuccessful = true
@@ -114,64 +135,65 @@ class myHandler(BaseHTTPRequestHandler):
         return
 
     def do_GET(self):
-        global myJ
+        global myJson
         self.configureResponse()
-        self.wfile.write(myJ.encode('utf-8'))
+        self.wfile.write(myJson.encode('utf-8'))
         return
 
     def do_POST(self):
-        global myJ
+        global myJson
         
         # Get length of data received
         contentLength = int(self.headers['Content-Length']) 
         
         # Get data received
         postData = self.rfile.read(contentLength) 
-        
-        functionRequest = str(self.path)
-        print("functionRequest: |" + functionRequest + "|")
-        #functionRequest = "diffsignup"
+        clientInfo = json.loads(postData.decode('utf-8'))
+       #functionRequest = str(self.path)
+        functionRequest = clientInfo['path']
 
         if functionRequest == "/signUp":
-            print("Sign up request received from client\n")
-            clientInfo = json.loads(postData.decode('utf-8'))
-            #signupLogic(clientInfo['username'],clientInfo['password'],clientInfo['usertype'])
-            configureJSON_signup(True,"-1","")
+            print("Sign up request received from client")
+            print("____________________________________")
+            
+            signupLogic(clientInfo['username'],clientInfo['password'],clientInfo['usertype'])
             self.configureResponse()
-            self.wfile.write(myJ.encode('utf-8'))
+            self.wfile.write(myJson.encode('utf-8'))
 
             return 
-        elif functionRequest == "signIn":
-            print("Sign in request received from client\n")
+        elif functionRequest == "/signIn":
+            print("Sign in request received from client")
+            print("____________________________________")
+            
             clientInfo = json.loads(postData.decode('utf-8'))
-            #signinLogic(clientInfo['username'],clientInfo['password'])
-            configureJSON_signin(True,"-1","no error message this time")
+            signinLogic(clientInfo['username'],clientInfo['password'])
             self.configureResponse()
-            self.wfile.write(myJ.encode('utf-8'))
+            self.wfile.write(myJson.encode('utf-8'))
 
             return
-        elif functionRequest == "sendAllUserData":
+        elif functionRequest == "/sendAllUserData":
             print("Send All User Data request received from client\n")
             clientInfo = json.loads(postData.decode('utf-8'))
             sendAllUserDataLogic(clientInfo['userAccessToken'])
             self.configureResponse()
-            self.wfile.write(myJ.encode('utf-8'))
+            self.wfile.write(myJson.encode('utf-8'))
 
             return
-        elif functionRequest == "userAssociatedClasses":
+        elif functionRequest == "/userAssociatedClasses":
             print("User Associated Classes request received from client\n")
             clientInfo = json.loads(postData.decode('utf-8'))
             userAssociatedClassesLogic(clientInfo['userAccessToken'])
             self.configureResponse()
-            self.wfile.write(myJ.encode('utf-8'))
+            self.wfile.write(myJson.encode('utf-8'))
 
             return  
         else:
             print("Different request received from client\n")
+            print(postData)
             #myJ = json.dumps({"Most recently bought plant" : "White Princess Philodendron", "Coffee Flavor Tonight" : "Toasted Almond", "Best Tequila" : "Tears of Llorona"})
-            myJ = json.dumps({"plant array" : ["White Princess Philodendron", "Pink Princess Philodendron", "Monstera Albo"], "int test":23})
+            myJson = json.dumps({"plant array" : ["White Princess Philodendron", "Pink Princess Philodendron", "Monstera Albo"], "int test":23})
             self.configureResponse()
-            self.wfile.write(myJ.encode('utf-8'))
+            self.wfile.write(myJson.encode('utf-8'))
 
         return
         
@@ -186,7 +208,7 @@ def run(server_class=HTTPServer, handler_class=myHandler, PORT=8080, HOST = "loc
     httpd.server_close()
 
 # Driver code for server
-PORT = 8080
-HOST = "192.168.1.9"
+PORT = 8888
+HOST = "192.168.1.7"
 print("Welcome to the TikEdu server! You are on port {}.\n".format(PORT))
 run(PORT = PORT, HOST = HOST)
